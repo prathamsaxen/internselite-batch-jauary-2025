770ebb451cec06aff92f985e5136e1f644f1b4b0
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-23 20:08:44 +0530

    Implemented Code for 23-01-2025

---
+++ 23-01-2025/script.js
@@ -0,0 +1,96 @@
+// console.log("Hello World!");
+// let x ='2+4;'
+// // let and const;
+// x = 10;
+// const value = 20;
+// console.log(value);
+// value=25;
+// console.log(value);
+// Variables and Const 
+// Var - ???
+// Global Scope and Block Scope
+// 
+
+// Double Qoutes or Single Quotes are considered as string.
+// String is anything which we write and wanted to use as it as is called string laterals.
+// {
+//     let a =5;
+//     var b =10;
+// }
+// console.log(b);
+
+// If else statement
+
+//  = -> Assign Operator
+// == and === equality comparison operator? - Data Type?
+
+
+// let sum = 0;
+// if(sum === 10)
+// {
+//     console.log("Money Sent to Vivek");
+// }
+// else if(sum > 10)
+// {
+//     console.log("Mutual Func me daal do.");
+// }
+// else
+// {
+//     console.log("To me kya karu aap to khud hi gareeb ho.");
+// }
+
+
+// Loop ?
+// console.log(1);
+// console.log(3);
+// console.log(5);
+// console.log(7);
+// console.log(9);
+
+// let max = 100;
+// // Loop is something which repeats your code n number of times with different iteration.
+// for(let index = 0; index<=max;index=index+1)
+// {
+//     if(index%2){
+//         console.log(index);
+//     }
+// }
+
+// Module Operator - 4 % 2 = 0
+// Coding 1 = true;
+//        0 = false;
+
+// 
+
+
+// Function Definition
+function sum(a,b,c){
+    console.log(a+b+c);
+}
+
+const subtract=(a,b)=>{
+    console.log(b-a);
+}
+
+
+// Function
+
+
+// let number = 8;
+// // 7 * 6 * 5 * 4 * 3 * 2
+
+// let index = number;
+// let factorial = 1;
+
+// while(index>1){
+//     factorial = factorial * index;
+//     index = index - 1;
+// }
+
+// console.log(factorial);
+
+// Function Call
+// sum(2,3,4);
+subtract(10,15);
+
+// sum(4,9,14);
