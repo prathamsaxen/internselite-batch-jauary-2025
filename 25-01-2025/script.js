d81b3164693552b6a1fc3a7a0e39b8ef843d4dfb
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-01-25 19:56:29 +0530

    Implemented SetTimeoUt and SetTimeInterval

--- 25-01-2025/script.js
+++ 25-01-2025/script.js
@@ -1,5 +1,7 @@
 let initialValue = 0;
 
+const date = new Date();
+
 let value = document.getElementById('value');
 value.innerHTML = initialValue;
 
@@ -26,3 +28,59 @@ function Decrease(){
 
 PlusButton.addEventListener('click', Increase);
 MinusButton.addEventListener('click',Decrease);
+
+// Array 
+let data = [1,2,3,"Pratham","Saxena"];
+console.log(data);
+
+// A = 25
+// B = 35
+// C = 0
+// D = 100
+// M = -1
+
+const arrData = [25,35,0,100,-1];
+            //   0  1  2  3   4
+        //   console.log(arrData[2])  
+
+        function averageCalculator(){
+            let sum = 0;
+            for(let index = 0;index<arrData.length;index++)
+            {
+                sum = sum + arrData[index];
+                //  sum+=arrData[index];
+            }
+            return sum/arrData.length;
+        }
+
+        console.log(averageCalculator())
+
+// String and Array are kind of similar thing or you can say exact same thing.
+
+// Objects => Key:Value
+let dataObject={
+    name:'Pratham Saxena',
+    occupation:'Software Developement Engineer |',
+}
+
+
+console.log(dataObject.occupation);
+console.log(date);
+
+
+function printDate(){
+    const display=document.getElementById('date-print');
+    const date = new Date();
+    display.innerHTML = date;
+}
+
+setInterval(()=>{
+    printDate();
+},5000)
+
+// SetInterval = Each Time Frame uske ander wala content ko dubara execute karega
+
+// document.getElementById('date-click').addEventListener('click',printDate)
+
+// SetTimeOut =  we use when koi particular task ko instantly execute na karake ek particular time frame ke baad execute karnaa ho 
+// SetInterval = Us tasko ko repeat krte rehna ek time frame ke 
